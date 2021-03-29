6d6d8e8c8b23ff9c76d5f470e71cc7105b2a8f59
Tomas Savcenko <to.savcenko@gmail.com>
2021-03-29 15:17:20 +0200

    Regular expression support

--- grep.py
+++ grep.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 import sys
+import re
 
 pattern = sys.argv[1]
 path = sys.argv[2]
 with open(path) as file:
     for line in file:
-        if pattern in line:
+        if re.search(pattern, line):
             print(line, end="")
 
