fc22e04b3ce184151192501d778bca58c34c934b
Tomas Savcenko <to.savcenko@gmail.com>
2021-04-02 22:56:03 +0200

    Error handling

--- grep.py
+++ grep.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python3
 
+"""Usage: grep.py PATTERN FILE
+
+Print lines from FILE matching regular expressions PATTERN
+
+"""
+
 import sys
 import re
 
-pattern = sys.argv[1]
-path = sys.argv[2]
-with open(path) as file:
-    for line in file:
-        if re.search(pattern, line):
-            print(line, end="")
+if len(sys.argv) == 3:
+    pattern = sys.argv[1]
+    path = sys.argv[2]
+else:
+    print(__doc__.strip(), file= sys.stderr)
+    sys.exit(1)
 
+try:
+    with open(path) as file:
+        for line in file:
+            if re.search(pattern, line):
+                print(line, end="")
+except FileNotFoundError as err:
+    print(__doc__.strip(), file= sys.stderr)
+    print(err, file= sys.stderr)
+    sys.exit(1)
