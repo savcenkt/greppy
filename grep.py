95e21716c4d945e81f055967118d4effa4c9f177
Tomas Savcenko <to.savcenko@gmail.com>
2021-04-03 19:19:47 +0200

    Improvemnt of code readability

--- grep.py
+++ grep.py
@@ -9,19 +9,32 @@ Print lines from FILE matching regular expressions PATTERN
 import sys
 import re
 
-if len(sys.argv) == 3:
-    pattern = sys.argv[1]
-    path = sys.argv[2]
-else:
-    print(__doc__.strip(), file= sys.stderr)
-    sys.exit(1)
+def grep(pattern, lines):
+    """Print lines matching pattern."""
+    for line in lines:
+        if re.search(pattern, line):
+             print(line, end="")
 
-try:
-    with open(path) as file:
-        for line in file:
-            if re.search(pattern, line):
-                print(line, end="")
-except FileNotFoundError as err:
-    print(__doc__.strip(), file= sys.stderr)
-    print(err, file= sys.stderr)
-    sys.exit(1)
+def parse_argv(argv):
+    """"Parce script arguments."""
+    if len(argv) == 3:
+        pattern = argv[1]
+        path = argv[2]
+    else:
+        print(__doc__.strip(), file= sys.stderr)
+        sys.exit(1)
+    return pattern, path
+
+def main():
+    """"Main entry point of script."""
+    pattern, path= parse_argv(sys.argv)
+    try:
+        with open(path) as file:
+            grep(pattern, file)
+    except FileNotFoundError as err:
+        print(__doc__.strip(), file= sys.stderr)
+        print(err, file= sys.stderr)
+        sys.exit(1)
+
+if __name__= "__name__":
+    main()
